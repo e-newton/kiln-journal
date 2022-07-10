ba100d1785765cb153821b95b08c12e94cc57650
Eric Newton <ericnewton@Erics-MacBook-Air.local>
2022-07-09 21:21:36 -0700

    Added in styling for the form input

--- src/journal-entry/JournalEntry.tsx
+++ src/journal-entry/JournalEntry.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import './JournalEntry.scss';
 
 function JournalEntry() {
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        console.log('swag');
+        event.preventDefault();
+    }
+
     return (
         <div className='JournalEntry'>
-
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Time</span>
+                    <input id='time' type={'time'}></input>
+                </label>
+                <label>
+                    <span>Temperature</span>
+                    <input type={'number'}></input>
+                </label>
+                <label>
+                    <span>Power</span>
+                    <input type={'number'}></input>
+                </label>
+                <label>
+                    <span>Notes</span>
+                    <span role="textbox" contentEditable></span>
+                </label>
+            </form>
         </div>
     )
 }
